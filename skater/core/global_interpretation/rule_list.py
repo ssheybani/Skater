5406934b22d9335dcff6ba54dd6f8b35361687a0
Pramit Choudhary <pramitchoudhary@Pramits-MacBook-Pro.local>
2017-10-26 20:24:29 -0700

    some more improvement to the test and initial construct

--- skater/core/global_interpretation/rule_list.py
+++ skater/core/global_interpretation/rule_list.py
@@ -1,22 +1,27 @@
 # Reference: https://github.com/Hongyuy/sbrl-python-wrapper/blob/master/sbrl/C_sbrl.py
-# Referencing the work done by Professor Cynthia/Hongyuy with modification
+# Referencing the work done by Professor Cynthia/Hongyuy/Others with modifications
 
 from rpy2.robjects.packages import importr
-from rpy2.robjects import pandas2ri
+from rpy2.robjects import pandas2ri, ro
+import numpy as np
+pandas2ri.activate()
 
 
 class SBRL():
     def __init__(self):
         self.r_sbrl = importr('sbrl')
         self.model = None
+        self.as_factor = ro.r['as.factor']
+        self.s_apply = ro.r['sapply']
+        self.as_factor = ro.r['data.frame']
 
 
-    def fit(self, X, y, **kwargs):
+    def fit(self, X, y_true, **kwargs):
         """
         Parameters:
-            X: pandas.DataFrame object, used to fit the model.
+            X: pandas.DataFrame object that could be used by the model for training.
                  It must not have a column named 'label'
-            y: pandas.Series, 1-D array to store ground truth labels
+            y_true: pandas.Series, 1-D array to store ground truth labels
             **kwarg: key word arguments including the following keys:
             'iters':    default 30000
             'pos_sign': default "1"
@@ -30,16 +35,19 @@ class SBRL():
             'nchain': default 10
 
         """
-        data = X.assign(label=y)
-        self.model = self.r_sbrl.sbrl(pandas2ri.py2ri(data), **kwargs)
+        data = X.assign(label=y_true)
+        data_as_r_frame = self.r_frame(self.s_apply(data, self.as_factor))
+        self.model = self.r_sbrl.sbrl(pandas2ri.py2ri(data_as_r_frame), **kwargs)
+        return self.model
 
 
-    def predict(self, X, type='prob'):
-        '''
-            `X`  an instance of pandas.DataFrame object, representing the data to be making predictions on.
+    def predict_prob(self, X):
+        """
+        Parameters:
+            X:  pandas.DataFrame object, representing the data to be making predictions on.
             `type`  whether the prediction is discrete or probabilistic.
             return a numpy.ndarray of shape (#datapoints, 2), the probability for each observations
-        '''
+        """
         results = self.r_sbrl.predict_sbrl(self.model, pandas2ri.py2ri(X))
         return np.asarray(map(pandas2ri.ri2py, results)).transpose()
 
--- skater/tests/test_rule_list.py
+++ skater/tests/test_rule_list.py
@@ -1,9 +1,30 @@
 import unittest
+import pandas as pd
+from skater.core.global_interpretation.rule_list import SBRL
+
 
 class TestRuleList(unittest.TestCase):
 
     def setup(self):
-        pass
+        self.sbrl_inst = SBRL()
+        self.input_data = pd.read_csv('data/sample.csv')
+        # data transformation and cleaning ...
+        self.input_data["Sex"] = self.input_data["Sex"].astype('category')
+        self.input_data["Sex_Encoded"] = self.input_data["Sex"].cat.codes
+        self.input_data["Embarked"] = self.input_data["Embarked"].astype('category')
+        self.input_data["Embarked_Encoded"] = self.input_data["Embarked"].cat.codes
+        self.input_data = self.input_data.drop(['Ticket','Cabin', 'Name', 'Sex', 'Embarked'], axis=1)
+        # Remove NaN values
+        self.input_data = self.input_data.dropna()
+        self.y = self.input_data['Survived']
+        self.input_data = self.input_data.drop(['Survived'], axis=1)
+
+
+    def test_model_build(self):
+        self.sbrl_inst.fit(self.input_data, self.y)
+        result_score = self.sbrl_inst.predict_prob(self.input_data)
+        print(result_score.shape)
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRuleList)
