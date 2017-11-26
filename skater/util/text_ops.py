802da0b2acb95f54dd55bc27e0d61e775ee27f94
Pramit Choudhary <pramitchoudhary@ip-172-30-0-2.us-west-2.compute.internal>
2017-11-26 14:51:15 -0800

    an example notebook(work_in_progress) for new features, and a new file for text specific ops

---
+++ skater/util/text_ops.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import re
+
+def preprocessor(text):
+    # remove HTML tags
+    text = BeautifulSoup(text, 'html.parser').get_text()
+
+    # regex for matching emoticons, keep emoticons, ex: :), :-P, :-D
+    r = '(?::|;|=|X)(?:-)?(?:\)|\(|D|P)'
+    emoticons = re.findall(r, text)
+    text = re.sub(r, '', text)
+
+    # convert to lowercase and append all emoticons behind (with space in between)
+    # replace('-','') removes nose of emoticons
+    text = re.sub('[\W]+', ' ', text.lower()) + ' ' + ' '.join(emoticons).replace('-','')
+    return text
