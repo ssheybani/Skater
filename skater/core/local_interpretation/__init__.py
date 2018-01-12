fafd8c0924a9ec5668ad3eaef646eb800e13cf76
Pramit Choudhary <pramitchoudhary@ip-172-30-0-3.us-west-2.compute.internal>
2018-01-11 20:53:41 -0800

    minor correction in exposing functions

--- skater/core/local_interpretation/__init__.py
+++ skater/core/local_interpretation/__init__.py
@@ -1,5 +1,4 @@
 from .text_interpreter import *
 
 __all__ = [relevance_wt_assigner, vectorize_as_tf_idf, get_feature_names, auto_feature_selection,
-           query_top_features_in_doc, query_top_features_overall, query_top_features_by_class,
-           convert_dataframe_to_dict, understand_estimator]
+           query_top_features_in_doc, query_top_features_overall, query_top_features_by_class, understand_estimator]
