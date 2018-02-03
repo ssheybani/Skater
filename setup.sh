fbbf410584d456f859ba4fd774939ef3758a9f41
Pramit Choudhary <pramitchoudhary@ip-172-30-0-2.us-west-2.compute.internal>
2018-02-02 16:49:58 -0800

    ability to support different os type with a simple flag for now

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Requirements: R >= 3.4.0
+conda install gxx_linux-64
+if [ "$1" = "mac" ]
+then
+    brew install r-cran-rcpp
+    # installing amp
+    brew install libgmp3-dev
+elif [ "$1" = "linux-ubuntu" ]
+then
+    sudo apt-get install r-cran-rcpp
+    # installing amp
+    sudo apt-get install libgmp3-dev
+elif [ "$1" = "linux-rpm" ]
+then
+    sudo apt-get install r-cran-rcpp
+    sudo yum install libgmp3-dev
+fi
+# downloads the required R packages locally in the same directory as setup.py
+wget https://cran.r-project.org/src/contrib/Rcpp_0.12.15.tar.gz
+R CMD INSTALL Rcpp_0.12.15.tar.gz
+wget https://cran.r-project.org/src/contrib/arules_1.5-5.tar.gz
+R CMD INSTALL arules_1.5-5.tar.gz
