dcdd653cbb137e6646546806dad7113eecc44603
Pramit Choudhary <pramitchoudhary@ip-172-30-0-2.us-west-2.compute.internal>
2018-02-09 17:26:12 -0800

    minor modification to the setup script

--- setup.sh
+++ setup.sh
@@ -3,17 +3,20 @@
 conda install gxx_linux-64
 if [ "$1" = "mac" ]
 then
+    brew install r
     brew install r-cran-rcpp
     # installing amp
     brew install libgmp3-dev
 elif [ "$1" = "linux-ubuntu" ]
 then
+    sudo apt-get -y install r-base
     sudo apt-get install r-cran-rcpp
     # installing amp
     sudo apt-get install libgmp3-dev
 elif [ "$1" = "linux-rpm" ]
 then
-    sudo apt-get install r-cran-rcpp
+    sudo yum -y install r-base
+    sudo yum install r-cran-rcpp
     sudo yum install libgmp3-dev
 fi
 # downloads the required R packages locally in the same directory as setup.py
