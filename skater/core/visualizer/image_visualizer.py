b6ba3a76a3cf0e96e7225079934943bf5673e934
Pramit Choudhary <pramitchoudhary@ip-172-30-0-2.us-west-2.compute.internal>
2018-04-05 22:49:22 -0700

    improvement to the image visualizer

--- skater/core/visualizer/image_visualizer.py
+++ skater/core/visualizer/image_visualizer.py
@@ -1,36 +1,39 @@
-from skimage import feature, transform
+from skimage.filters import roberts, sobel
 import numpy as np
 import matplotlib.pyplot as plt
 
+from skater.util.image_ops import normalize
 
-def visualize(input_img, xi=None, cmap='bwr', axis=plt, percentile=100, dilation=3.0, alpha=0.8):
+
+def visualize(relevance_score, original_input_img=None, edge_detector_alg='robert', cmap='bwr', axis=plt,
+              percentile=100, alpha=0.8):
+    # Normalize the input image to (0,1)
+    xi = normalize(original_input_img)
+
     dx, dy = 0.01, 0.01
-    xx = np.arange(0.0, input_img.shape[1], dx)
-    yy = np.arange(0.0, input_img.shape[0], dy)
-    xmin, xmax, ymin, ymax = np.amin(xx), np.amax(xx), np.amin(yy), np.amax(yy)
-    extent = xmin, xmax, ymin, ymax
-    cmap_xi = plt.get_cmap('Greys_r')
-    cmap_xi.set_bad(alpha=0)
-    overlay = None
-    if xi is not None:
-        # Compute edges (to overlay to heatmaps later)
-        xi_greyscale = xi if len(xi.shape) == 2 else np.mean(xi, axis=-1)
-        in_image_upscaled = transform.rescale(xi_greyscale, dilation, mode='constant')
-        edges = feature.canny(in_image_upscaled).astype(float)
-        edges[edges < 0.5] = np.nan
-        edges[:5, :] = np.nan
-        edges[-5:, :] = np.nan
-        edges[:, :5] = np.nan
-        edges[:, -5:] = np.nan
-        overlay = edges
+    xx = np.arange(0.0, relevance_score.shape[1], dx)
+    yy = np.arange(0.0, relevance_score.shape[0], dy)
 
-    abs_max = np.percentile(np.abs(input_img), percentile)
+    x_min, x_max, y_min, y_max = np.amin(xx), np.amax(xx), np.amin(yy), np.amax(yy)
+    extent = x_min, x_max, y_min, y_max
+    xi_cmap = plt.cm.gray
+    xi_cmap.set_bad(alpha=0)
+
+    if xi is not None:
+        xi_greyscale = xi if len(xi.shape) == 2 else np.mean(xi, axis=-1)
+        # Applying edge detection ( Roberts or Sobel edge detection )
+        # Reference: http://scikit-image.org/docs/0.11.x/auto_examples/plot_edge_filter.html
+        edge_detector = {'robert': roberts, 'sobel': sobel}
+        edges = edge_detector[edge_detector_alg](xi_greyscale)
+
+    abs_max = np.percentile(np.abs(relevance_score), percentile)
     abs_min = abs_max
 
-    input_img = input_img[:, :, 0] if len(input_img.shape) == 3 else input_img
+    relevance_score = relevance_score[:, :, 0] if len(relevance_score.shape) == 3 else relevance_score
+    axis.imshow(relevance_score, extent=extent, interpolation='nearest', cmap=cmap, vmin=-abs_min, vmax=abs_max)
 
-    axis.imshow(input_img, extent=extent, interpolation='nearest', cmap=cmap, vmin=-abs_min, vmax=abs_max)
-    if overlay is not None:
-        axis.imshow(overlay, extent=extent, interpolation='nearest', cmap=cmap_xi, alpha=alpha)
+    # draw the edges of the image before overlaying the learned relevance
+    if edges is not None:
+        axis.imshow(edges, extent=extent, interpolation='nearest', cmap=xi_cmap, alpha=alpha)
     axis.axis('off')
     return axis
